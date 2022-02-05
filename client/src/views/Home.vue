67f72b0eb808542bbb902b5bcf1a3490c97cd2e9
Adrian Grochowski <adrian.grochowski@makolab.com>
2022-02-05 14:02:06 +0100

    setting up the registration page

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Song tracker</h1>
+    <router-link to="/Register">Register</router-link>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <v-card title="Register">
+        <form 
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async register () {
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data);
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
